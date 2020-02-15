2c86c110be318d2ee0f30b30a7d580a3a605416e
robertoramosrc <robertoramos.net@hotmail.com>
2020-02-15 14:22:49 -0300

    retirar comentarios

--- projects/strategy/src/main/java/br/com/beta1/model/CalculadoraDeImpostos.java
+++ projects/strategy/src/main/java/br/com/beta1/model/CalculadoraDeImpostos.java
@@ -4,16 +4,8 @@ import java.math.BigDecimal;
 
 public class CalculadoraDeImpostos {
 
-    //problema que foi refatorado
-        /*if( "ICMS".equals(imposto) ) {
-           double icms = new ICMS().calculaICMS(orcamento);
-        } else if( "ISS".equals(imposto) ) {
-            double iss = new ISS().calculaISS(orcamento);
-        }*/
-
     public BigDecimal calculaImposto(Orcamento orcamento, InterfaceImposto imposto) {
         return imposto.calculaImposto(orcamento.getValor());
     }
-
 
 }
